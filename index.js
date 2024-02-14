da1e46eda936ace08a8f79a0773ca497fe52efef
WislyHubHub <rachapruek12345@gmail.com>
2024-02-14 10:22:53 +0700

    qeqwewq

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ var bodyParser = require('body-parser');
 const base_url = "http://node58383-wislynoderest.proen.app.ruk-com.cloud";
 
 
-app.set("views" ,path.join(__dirname, "/public/views"));
+app.set('views' ,path.join(__dirname, '/public/views'));
 app.set('view engine' ,'ejs');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false}));
